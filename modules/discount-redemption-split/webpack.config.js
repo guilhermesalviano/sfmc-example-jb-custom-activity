c1706dcb037216fa31ca4b0ead5d1f023d300f6d
Guilherme Salviano <guilhermesalviano@Mac-mini-de-Guilherme.local>
2023-11-29 20:47:45 -0300

    fix: webpack error

--- modules/discount-redemption-split/webpack.config.js
+++ modules/discount-redemption-split/webpack.config.js
@@ -27,8 +27,8 @@ module.exports = function(env, argv) {
                 {
                     // you may want to bundle SLDS SASS files with webpack,
                     // we'll keep things simple for this example and just copy SLDS into dist
-                    //from: path.resolve(__dirname, '../../node_modules/@salesforce-ux/design-system/assets'),
-                    //to: path.resolve(__dirname, 'dist/design-system')
+                    from: path.resolve(__dirname, '../../node_modules/@salesforce-ux/design-system/assets'),
+                    to: path.resolve(__dirname, 'dist/design-system')
                 },
             ]),
             new webpack.BannerPlugin(
